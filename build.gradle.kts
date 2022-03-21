4a07eb35cd14734d8422b9890a54c4de873f4a2b
VINTO1819 <develretr0@gmail.com>
2022-03-21 14:20:18 +0900

    🔧:: Gradle 빌드 문제 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
     application
+    kotlin("jvm") version "1.5.10"
 }
 
 group = "me.minie"
@@ -12,12 +12,21 @@ repositories {
     mavenCentral()
 }
 
-
-
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.jar {
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.INCLUDE
+    manifest {
+        attributes(mapOf("Main-Class" to "MainKt"))
+    }
+
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
 }
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+# move execute script on /bin directory
 sudo cp fennec /bin/
-sudo cp fennec-git.jar /bin/
 
+# build jar and copy to /bin directory
+./gradlew jar
+sudo cp ./build/libs/fennec-git-1.0-SNAPSHOT.jar /bin/fennec.jar
+
+# give permissions
 sudo chmod 777 /bin/fennec
-sudo chmod 666 /bin/fennec-git.jar
+sudo chmod 666 /bin/fennec.jar
