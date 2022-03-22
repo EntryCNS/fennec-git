ede5807b51a248c99618438531f3576614a325a2
VINTO1819 <develretr0@gmail.com>
2022-03-22 11:39:05 +0900

    🖋️:: 명령어 개별 도움말 제공

--- src/main/kotlin/commands/help/HelpCommand.kt
+++ src/main/kotlin/commands/help/HelpCommand.kt
@@ -1,18 +1,27 @@
 package commands.help
 
-import commands.CommandHelper
-import commands.FennecArgument
-import commands.FennecCommand
+import commands.*
 
 class HelpCommand: FennecCommand() {
     override fun getName(): String = "help"
 
     override fun getDescription(): String = "도움말을 출력합니다"
 
-    override fun getRequiredArguments(): List<FennecArgument> = listOf()
+    override fun getRequiredArguments(): List<FennecArgument> = listOf(
+        FennecArgument("명령어 이름", listOf(ArgumentType.STRING), true)
+    )
 
     override fun run(arguments: List<String>) {
-        CommandHelper.printHelp()
+        when(arguments.size) {
+            1 -> {
+                val query = arguments[0]
+                val command = CommandMapper.map[query]
+                if(command != null) {
+                    CommandHelper.printCommandHelp(command)
+                }else println("명령어 '${query}'을/를 찾을 수 없습니다")
+            }
+            else -> CommandHelper.printHelp()
+        }
     }
 
 }
